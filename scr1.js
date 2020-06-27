a37e037892ca3ac55e31b16afc36de02ebcac5c8
Strelok-SV <streloksv036@gmail.com>
2020-06-27 13:07:50 +0300

    6

--- scr1.js
+++ scr1.js
@@ -1,11 +1,3 @@
 "use strict";
-function imDig(Im, Rel) {
-    return {
-        Im: Im + "i",
-        Rel,
-        valueOf() { return this.Rel },
-        toString() { return this.Im }
-    }
-};
-let first = imDig(3, 4);
-alert(first);
+function f();
+alert(typeof f.name);
